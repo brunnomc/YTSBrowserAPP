4ec764955512ba87fe456b668f756ffcec011888
Brunno Melo Conrado <brunno.conrado@SDA.CE.GOV.BR>
2018-02-05 12:11:24 -0300

    Refactoring SearchActivity and show progressbar while querying

--- YTSBrowserApp/app/src/main/java/com/ytsbrowserapp/ytsbrowserapp/activity/SearchActivity.kt
+++ YTSBrowserApp/app/src/main/java/com/ytsbrowserapp/ytsbrowserapp/activity/SearchActivity.kt
@@ -19,64 +19,108 @@ import retrofit2.Response
 class SearchActivity : AppCompatActivity() {
 
     var movies: List<Movie> = emptyList()
+    var call: Call<MoviesResponse> = ServiceFactory().movieListService().getDefaultMovieList(queryTerm = "", limit = 50)
+
+    private enum class QueryStatus(val status: String) {
+        RUNNING("running"), RESPONSE_OK("responseOk"), RESPONSE_EMPTY("responseEmpty"), FAILURE("failure"), NO_QUERY("noQuery")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         setSupportActionBar(toolbar)
-        val adapter = MoviesListAdapter(movies, this)
-        recycler_view_movies.layoutManager = GridLayoutManager(this@SearchActivity, adapter.getNumberOfCovers())
-        recycler_view_movies.adapter = adapter
-
-        search_view.setOnQueryTextListener(object : OnQueryTextListener {
-            override fun onQueryTextChange(newText: String): Boolean {
-
-                if (newText.isNotEmpty()) {
-                    val call = ServiceFactory().movieListService().getDefaultMovieList(queryTerm = newText, limit = 50)
-                    call.enqueue(object : Callback<MoviesResponse> {
-                        override fun onResponse(call: Call<MoviesResponse>?, response: Response<MoviesResponse>?) {
-
-                            if (response!!.body()!!.data.movieCount > 0) {
-                                recycler_view_movies.visibility = View.VISIBLE
-                                relative_layout_query_message.visibility = View.GONE
-                                movies = response.body()!!.data.movies
-                                recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
-                            } else {
-                                recycler_view_movies.visibility = View.GONE
-                                relative_layout_query_message.visibility = View.VISIBLE
-                                text_view_query_message.text = resources.getString(R.string.search_activity_no_results)
-                                movies = emptyList()
-                                recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
-                            }
-                        }
-
-                        override fun onFailure(call: Call<MoviesResponse>?, t: Throwable?) {
-
-                        }
-                    })
-                } else {
-                    movies = emptyList()
-                    recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
-                    recycler_view_movies.visibility = View.GONE
-                    relative_layout_query_message.visibility = View.VISIBLE
-                    text_view_query_message.text = resources.getString(R.string.search_activity_type_to_search)
-                }
-
-
-                return false
-            }
-
-            override fun onQueryTextSubmit(query: String): Boolean {
-                //Task HERE
-                return false
-            }
-
-        })
-
         if (supportActionBar != null) {
             supportActionBar!!.setDisplayShowHomeEnabled(true)
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         }
+        setupMovieRecyclerView()
+
+        setupSearchView()
+    }
+
+    private fun setupSearchView() {
+        search_view.setOnQueryTextListener(object : OnQueryTextListener {
+            override fun onQueryTextChange(newText: String): Boolean {
+                setupViewByQueryStatus(QueryStatus.RUNNING)
+                if (newText.isNotEmpty()) {
+                    call = ServiceFactory().movieListService().getDefaultMovieList(queryTerm = newText, limit = 50)
+                    call.enqueue(object : Callback<MoviesResponse> {
+                        override fun onResponse(call: Call<MoviesResponse>?, response: Response<MoviesResponse>?) {
+                            if (response!!.body()!!.data.movieCount > 0) {
+                                movies = response.body()!!.data.movies
+                                setupViewByQueryStatus(QueryStatus.RESPONSE_OK)
+                            } else {
+                                movies = emptyList()
+                                setupViewByQueryStatus(QueryStatus.RESPONSE_EMPTY)
+                            }
+                        }
+
+                        override fun onFailure(call: Call<MoviesResponse>, t: Throwable?) {
+                            if (!call.isCanceled) {
+                                setupViewByQueryStatus(QueryStatus.FAILURE)
+                            }
+                        }
+                    })
+                } else {
+                    setupViewByQueryStatus(QueryStatus.NO_QUERY)
+                }
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean = false
+
+        })
+    }
+
+    private fun setupViewByQueryStatus(queryStatus: QueryStatus) {
+        when (queryStatus) {
+            QueryStatus.RUNNING -> {
+                call.cancel()
+
+                recycler_view_movies.visibility = View.GONE
+                relative_layout_query_message.visibility = View.VISIBLE
+                text_view_query_message.visibility = View.GONE
+                progress_bar_searching.visibility = View.VISIBLE
+            }
+            QueryStatus.NO_QUERY -> {
+                movies = emptyList()
+                recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
+
+                recycler_view_movies.visibility = View.GONE
+                relative_layout_query_message.visibility = View.VISIBLE
+                text_view_query_message.visibility = View.VISIBLE
+                text_view_query_message.text = resources.getString(R.string.search_activity_type_to_search)
+                progress_bar_searching.visibility = View.GONE
+            }
+            QueryStatus.RESPONSE_OK -> {
+                recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
+
+                recycler_view_movies.visibility = View.VISIBLE
+                relative_layout_query_message.visibility = View.GONE
+            }
+            QueryStatus.RESPONSE_EMPTY -> {
+                recycler_view_movies.adapter = MoviesListAdapter(movies, this@SearchActivity)
+
+                recycler_view_movies.visibility = View.GONE
+                relative_layout_query_message.visibility = View.VISIBLE
+                text_view_query_message.visibility = View.VISIBLE
+                text_view_query_message.text = resources.getString(R.string.search_activity_no_results)
+                progress_bar_searching.visibility = View.GONE
+            }
+            QueryStatus.FAILURE -> {
+                recycler_view_movies.visibility = View.GONE
+                relative_layout_query_message.visibility = View.VISIBLE
+                text_view_query_message.visibility = View.VISIBLE
+                text_view_query_message.text = resources.getString(R.string.search_activity_failure)
+                progress_bar_searching.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupMovieRecyclerView() {
+        val adapter = MoviesListAdapter(movies, this)
+        recycler_view_movies.layoutManager = GridLayoutManager(this@SearchActivity, adapter.getNumberOfCovers())
+        recycler_view_movies.adapter = adapter
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
